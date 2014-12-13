af14a6ac7a989ca0f165be333b66004976a81180
Victoria Marafioti <vicmarf5@aol.com>
2014-12-13 17:56:55 -0500

    working robot

--- src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -8,7 +8,10 @@
 package edu.wpi.first.wpilibj.templates;
 
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
+import edu.wpi.first.wpilibj.Timer;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -18,9 +21,11 @@ import edu.wpi.first.wpilibj.SimpleRobot;
  * directory.
  */
 public class RobotTemplate extends SimpleRobot {
-    /**
-     * This function is called once each time the robot enters autonomous mode.
-     */
+      Joystick leftJoyStick = new Joystick(1);
+      Joystick rightJoyStick = new Joystick(2);
+      RobotDrive robotDrive = new RobotDrive(4, 3, 2, 1);
+      
+      
     public void autonomous() {
         
     }
@@ -29,7 +34,14 @@ public class RobotTemplate extends SimpleRobot {
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
-
+       robotDrive.setSafetyEnabled(true); 
+       
+       while(isOperatorControl() && isEnabled()) {
+           robotDrive.tankDrive(leftJoyStick, rightJoyStick);
+           
+           Timer.delay(0.01);
+       }
+        
     }
     
     /**
